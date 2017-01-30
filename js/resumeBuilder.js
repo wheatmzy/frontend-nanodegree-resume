697b4b55fde2d895cc0e3fa7f33eba72b450a177
Wheat <Wheat>
2017-01-30 18:30:27 -0500

    finished first class and problem set zero; add a few test elements to resumebuilder.js;

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,10 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ $('#main').append('Emrys');
+ var light,result; 
+ light = 299792458;
+ result = light * 100 * (1/1000000000);
+ console.log(result);
+ 
